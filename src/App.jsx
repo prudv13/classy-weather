2a3efb0a9abde5ffe5af688ae27d66fd18510871
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-09 16:43:04 +0530

    displaying the weather

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import './App.css'
 function getWeatherIcon(wmoCode) {
   const icons = new Map([
     [[0], "☀️"],
-    [[1], "🌤"],
-    [[2], "⛅️"],
+    [[1], "🌤️"],
+    [[2], "⛅"],
     [[3], "☁️"],
-    [[45, 48], "🌫"],
-    [[51, 56, 61, 66, 80], "🌦"],
-    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
-    [[71, 73, 75, 77, 85, 86], "🌨"],
-    [[95], "🌩"],
-    [[96, 99], "⛈"],
+    [[45, 48], "💨"],
+    [[51, 56, 61, 66, 80], "🌦️"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧️"],
+    [[71, 73, 75, 77, 85, 86], "🌨️"],
+    [[95], "🌩️"],
+    [[96, 99], "⛈️"],
   ]);
 
   const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
@@ -91,9 +91,60 @@ class App extends Component {
         </div>
         <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className='loader'>Loading...</p>}
+        {
+          this.state.weather.weathercode && 
+          <Weather
+            weather={this.state.weather}
+            location={this.state.displayLocation}
+          />
+        }
       </div>
     )
   }
 }
 
 export default App
+
+class Weather extends Component {
+  render(){
+    const {
+      temperature_2m_max: max, 
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather
+    return (
+      <div>
+        <h2>Weather {this.props.location}</h2>
+        <ul className='weather'>
+          {
+            dates.map((date,i) => (
+                <Day 
+                  date={date} 
+                  max={max.at(i)}
+                  min={min.at(i)}
+                  code={codes.at(i)}
+                  key={date}
+                  isToday={i===0}
+                />
+              )
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+class Day extends Component {
+  render() {
+    const {date, max, min, code, isToday} = this.props;
+    return (
+      <li className='day'>
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? "Today" : formatDay(date)}</p>
+        <p>{Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong></p>
+      </li>
+    )
+  }
+}
