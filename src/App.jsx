7845a2ec9ae7cf5d2e96f6f78699c797a07f73f9
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-09 15:56:58 +0530

    implemented form controlled component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,32 @@ import { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      location: 'lisbon'
+    }
+    this.fetchWeather = this.fetchWeather.bind(this)
+  }
+
+  fetchWeather(){
+    console.log('Loading data...')
+    console.log(this);
+  }
+
   render() {
     return (
-      <div>
+      <div className='app'>
         <h1>Classy Weather</h1>
+        <div>
+          <input 
+            type='text'
+            placeholder='Search from location...'
+            value={this.state.location}
+            onChange={e => this.setState({location: e.target.value})}
+          />
+        </div>
+        <button onClick={this.fetchWeather}>Get weather</button>
       </div>
     )
   }
